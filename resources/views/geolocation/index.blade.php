950551558140531ed25821fd7f3f3fbddea913d8
Roman Khomenko <pastorixx@mail.ru>
2017-06-25 13:43:00 +0600

    Added test geolocation script

---
+++ resources/views/geolocation/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.main')
+
+@section('content')
+
+
+
+<script type="text/javascript">
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        console.log(position);
+        /*
+        position = {
+            coords: {
+                latitude - Широта.
+                longitude - Долгота.
+                altitude - Высота в метрах над уровнем моря.
+                accuracy - Погрешность в метрах.
+                altitudeAccuracy - Погрешность высоты над уровнем моря в метрах.
+                heading - Направление устройства по отношению к северу.
+                speed - Скорость в метрах в секунду.
+            }
+            timestamp - Время извлечения информации.
+        }
+        */
+
+    },
+    function(error){
+
+        /*
+        error = {
+            code - Тип ошибки
+                    1 - PERMISSION_DENIED
+                    2 - POSITION_UNAVAILABLE
+                    3 - TIMEOUT
+
+            message - Детальная информация.
+        }
+        */
+
+    }
+);
+</script>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,5 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ApplicationController@index');
+Route::get('geolocation', 'GeolocationController@index');
